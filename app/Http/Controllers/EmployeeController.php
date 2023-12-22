cc2e6c2f8910a9916762ff5cc32ecfaea01654bc
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-12-22 16:54:46 +0800

    more crud functionalities

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Employee;
+use Illuminate\Contracts\Session\Session;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -27,5 +28,43 @@ class EmployeeController extends Controller
     public function create()
     {
         return view('employees.create');
+    }
+
+    //store employee
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'name' => 'required|string|max:255',
+            'phone_number' => 'required|numeric|digits:10',
+            'email' => 'required|email|max:255',
+            'hire_date' => 'required',
+            'type' => 'required',
+        ]);
+
+        Employee::create($formFields);
+
+        return redirect('/')->with('message', 'Employee created successfully!');
+    }
+
+    //edit form
+    public function edit(Employee $employee)
+    {
+        return view('employees.edit', ['employee' => $employee]);
+    }
+
+    //update employee
+    public function update(Request $request, Employee $employee)
+    {
+        $formFields = $request->validate([
+            'name' => 'required|string|max:255',
+            'phone_number' => 'required|numeric|digits:10',
+            'email' => 'required|email|max:255',
+            'hire_date' => 'required',
+            'type' => 'required',
+        ]);
+
+        $employee->update($formFields);
+
+        return redirect('/employees/' . $employee->id)->with('message', 'Employee updated successfully!');
     }
 }
