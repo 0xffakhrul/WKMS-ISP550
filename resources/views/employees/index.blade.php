b9ffd65b429698ccb41edc12980204c97f5aa7cc
Fakhrul Radzi <fakhrulradzi200@gmail.com>
2023-12-22 16:58:47 +0800

    use datatable in the employees list

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -5,6 +5,32 @@
 @endpush
 
 @section('content')
+    @php
+        $heads = ['ID', 'Name', 'Email', 'Phone No.', 'Type', ['label' => 'Actions', 'no-export' => true, 'width' => 5]];
+
+        // Retrieve data from the database using Eloquent
+        $employees = \App\Models\Employee::all();
+
+        // Build the data array for the datatable
+        $data = [];
+        foreach ($employees as $employee) {
+            $type = $employee->type == 'full_time' ? 'Full-Time' : 'Part-Time';
+            $btnDetails = '<a href="/employees/' . $employee->id . '"><i class="fas fa-fw fa-eye"></i></a>';
+            $btnEdit = '<a href=""><i class="fas fa-fw fa-pencil-alt text-warning"></i></a>';
+            $btnDelete = '<a href=""><i class="fas fa-fw fa-trash text-danger"></i></a>';
+
+            $rowData = [$employee->id, $employee->name, $employee->email, $employee->phone_number, $type, '<nobr>' . $btnDetails . $btnEdit . $btnDelete . '</nobr>'];
+            $data[] = $rowData;
+        }
+
+        $config = [
+            'data' => $data,
+            'order' => [[1, 'desc']],
+            'columns' => [null, null, null, ['orderable' => false]],
+            // 'searching' => true,
+        ];
+    @endphp
+
     <section class="container-fluid pt-3">
         <div class="d-flex justify-content-between align-items-center">
             <h1 class="fw-bold">Employees</h1>
@@ -13,24 +39,21 @@
         <section class="content">
             <div class="box">
                 <div class="box-body">
-                    <table class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Name</th>
-                                <th>Type</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($employees as $employee)
-                                <x-employee-table :employee="$employee" />
-                            @endforeach
-                        </tbody>
-                    </table>
+                    <div class="card">
+                        <div class="card-body">
+                            <x-adminlte-datatable id="table1" :heads="$heads" hoverable>
+                                @foreach ($config['data'] as $row)
+                                    <tr>
+                                        @foreach ($row as $cell)
+                                            <td>{!! $cell !!}</td>
+                                        @endforeach
+                                    </tr>
+                                @endforeach
+                            </x-adminlte-datatable>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
     </section>
-
 @stop
